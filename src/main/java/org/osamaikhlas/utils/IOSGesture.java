9b288350409cbf5b794ea605460ce691cb596e0c
osamaikhlas <osama.ikhlas@gmail.com>
2024-11-07 16:46:00 +0500

    code refactoring

--- src/main/java/org/osamaikhlas/utils/IOSGesture.java
+++ src/main/java/org/osamaikhlas/utils/IOSGesture.java
@@ -5,12 +5,13 @@ import java.util.Map;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebElement;
 import org.testng.Assert;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 import io.appium.java_client.ios.IOSDriver;
 
-public class IOSGesture extends AppiumUtils {
-	
+public class IOSGesture extends AppiumUtils { private static final Logger logger = LoggerFactory.getLogger(IOSGesture.class);
 IOSDriver driver;
 
 	
@@ -20,7 +21,9 @@ IOSDriver driver;
 	
 
 public void longPressIOS(WebElement ele, int duration) {
-		
+		if (ele == null) {
+		throw new IllegalArgumentException("WebElement cannot be null");
+		}
 		Map <String,Object>params = new HashMap<>();
 		params.put("element", ((RemoteWebElement)ele).getId());
 		params.put("duration", duration);
@@ -29,7 +32,9 @@ public void longPressIOS(WebElement ele, int duration) {
 	}
 	
 	public void scrollIOS(WebElement ele, String direction) {
-		
+		if (ele == null) {
+			throw new IllegalArgumentException("WebElement cannot be null");
+		}
 		Map <String,Object>params = new HashMap<>();
 		params.put("element", ((RemoteWebElement)ele).getId());
 		params.put("direction", direction);
@@ -38,9 +43,16 @@ public void longPressIOS(WebElement ele, int duration) {
 	}
 	
 	public void sliderIOS(WebElement ele, String percent) {
-		ele.sendKeys(percent); //setValue
-		Assert.assertEquals(percent, ele.getAttribute("value"));
-		
+		if (ele == null) {
+			throw new IllegalArgumentException("WebElement cannot be null");
+		}
+		try {
+			ele.sendKeys(percent); //setValue
+			Assert.assertEquals(percent, ele.getAttribute("value"));
+		} catch (Exception e) {
+			System.err.println("Error setting slider value: " + e.getMessage());
+			// Additional logging or error handling can be added here
+		}
 	}
 	
 	public void launchApp(String bundleId) {
@@ -48,10 +60,11 @@ public void longPressIOS(WebElement ele, int duration) {
 		Map <String,Object>params = new HashMap<>();
 		params.put("bundleId", bundleId);
 		driver.executeScript("mobile:launchApp", params);
-		
+		logger.info("App launched with bundleId: {}", bundleId);
+
+
 	}
 	public void swipeIOS( String direction) {
-		
 		Map <String,Object>params = new HashMap<>();
 		params.put("direction", direction);
 		driver.executeScript("mobile:swipe", params);
